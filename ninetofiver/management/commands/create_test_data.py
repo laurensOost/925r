92c5e6a74afbee84d7d50a6dff205b3477422e34
Denis Kristak <thenis@inuits.eu>
2021-05-10 11:00:11 +0200

    progress on test db data population

--- ninetofiver/management/commands/create_test_data.py
+++ ninetofiver/management/commands/create_test_data.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from ninetofiver.test_db_populator import populate_basic_tables
+from ninetofiver.test_db_populator import populate_basic_tables, populate_performance_tables
 
 
 class Command(BaseCommand):
@@ -11,3 +11,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Create a new timesheet for the current month for each user."""
         populate_basic_tables()
+        populate_performance_tables()
--- ninetofiver/test_db_populator.py
+++ ninetofiver/test_db_populator.py
@@ -1,12 +1,17 @@
-from ninetofiver.models import LeaveType, Company, Location, PerformanceType, WorkSchedule
+from ninetofiver.models import LeaveType, Company, Location, PerformanceType, WorkSchedule, ContractRole,\
+    ContractUser, Contract, ContractGroup, Performance
+from django.contrib.auth.models import User
+import ninetofiver.management.commands.create_timesheets
 import logging
+from django.conf import settings
+import datetime
+import random
 log = logging.getLogger(__name__)
 
 
 # this fills basic tables with data for debugging purposes
 # when modifying, make sure to change all lists related to the object
 def populate_basic_tables():
-    from django.conf import settings
     if not settings.DEBUG:
         log.error('settings.DEBUG is False. Aborting')
         exit(1)
@@ -82,3 +87,110 @@ def populate_basic_tables():
             sunday=ws_sunday
         )
         ws.save()
+
+
+# fills higher amount of test data to all contract/performance-related tables
+def populate_performance_tables():
+    if not settings.DEBUG:
+        log.error('settings.DEBUG is False. Aborting')
+        exit(1)
+
+    users = []
+    companies = []
+    companies_customers = []
+    contracts = []
+    contract_users = []
+    contract_groups = []
+    contract_roles = []
+
+    # add users, companies, customers, contract_groups
+    for x in range(1, 1001):
+        cr = ContractRole(
+            name='test_contract_role_' + str(x),
+            description='test_contract_role_' + str(x)
+        )
+        contract_roles.append(cr)
+        cr.save()
+
+        usr = User(
+            username='test_user_' + str(x),
+            email='test@mail.com'
+        )
+        users.append(usr)
+        usr.save()
+
+        comp = Company(
+            vat_identification_number='CZ' + str(x),
+            name='test_company_' + str(x),
+            address='test_company_' + str(x),
+            country='CZ',
+            internal=True,
+        )
+        companies.append(comp)
+        comp.save()
+
+        comp_cus = Company(
+            vat_identification_number='CZ_cus_' + str(x),
+            name='test_company_customer_' + str(x),
+            address='test_company_customer_' + str(x),
+            country='CZ',
+            internal=False,
+        )
+        companies_customers.append(comp_cus)
+        comp_cus.save()
+
+        # ContractGroup
+        cg = ContractGroup(
+            name='test_contract_group_' + str(x)
+        )
+        contract_groups.append(cg)
+        cg.save()
+
+    for x in range(1, 10001):
+        ctr = Contract(
+            name='test_contract_' + str(x),
+            description='test_contract_' + str(x),
+            customer=companies_customers.pop(x % 10),
+            company=companies.pop(x % 10),
+            starts_at=get_random_date(datetime.date(2017, 1, 1), datetime.date(2021, 1, 1)),
+            ends_at=get_random_date(datetime.date(2021, 9, 9), datetime.date(2030, 1, 1)),
+            active=1,
+            # performance_types=PerformanceType.objects.get(pk=1),
+            # contract_groups=contract_groups.pop(x % 10),
+            # contract_users=contract_users.pop(x % 10)
+        )
+        # ctr.contract_groups.set(contract_groups.pop(x % 10))
+        # ctr.contract_users.set(contract_users.pop(x % 10))
+        # ctr.performance_types.set(PerformanceType.objects.get(pk=1))
+        contracts.append(ctr)
+        ctr.save()
+
+        cu = ContractUser(
+            user=users.pop(x % 1000),
+            contract=ctr,
+            contract_role=contract_roles.pop(x % 1000)
+        )
+        contract_users.append(cu)
+        cu.save()
+
+    ninetofiver.management.commands.create_timesheets.Command.handle()
+
+    # # Performance
+    # for x in range(1, 1001):
+    #     pfr = Performance(
+    #         timesheet=
+    #         date=
+    #         contract=
+    #         redmine_id=
+    #     )
+    #     pfr.save()
+
+
+def get_random_date(start_date, end_date):
+    time_between_dates = end_date - start_date
+    days_between_dates = time_between_dates.days
+
+    random_number_of_days = random.randrange(days_between_dates)
+    random_date = start_date + datetime.timedelta(days=random_number_of_days)
+
+    return random_date
