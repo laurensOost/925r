e73e4b04c54cd973d06703e6bc0af36699a00063
Pavel Tomanek <pavel.tomanek@inuits.eu>
2023-06-05 13:47:04 +0200

    fix - when user worked for another company #refs 67341

--- ninetofiver/management/commands/send_staff_pending_leave_reminders.py
+++ ninetofiver/management/commands/send_staff_pending_leave_reminders.py
@@ -1,5 +1,6 @@
 """Send staff a reminder about pending leaves."""
 import logging
+from datetime import date
 from django.db.models import Q
 from django.core.management.base import BaseCommand
 from django.contrib.auth import models as auth_models
@@ -14,36 +15,54 @@ log = logging.getLogger(__name__)
 class Command(BaseCommand):
     """Send staff a reminder about pending leave."""
 
-    args = ''
-    help = 'Send staff a reminder about pending leave'
+    args = ""
+    help = "Send staff a reminder about pending leave"
 
     def handle(self, *args, **options):
         """Send staff a reminder about pending leave."""
         pending_leaves = models.Leave.objects.filter(status=models.STATUS_PENDING)
         pending_leave_count = pending_leaves.count()
-        log.info('%s pending leave(s) found' % pending_leave_count)
+        log.info("%s pending leave(s) found" % pending_leave_count)
 
         if pending_leave_count:
-            recipients = get_users_with_permission(models.PERMISSION_RECEIVE_PENDING_LEAVE_REMINDER)
-
+            recipients = get_users_with_permission(
+                models.PERMISSION_RECEIVE_PENDING_LEAVE_REMINDER
+            )
             for recipient in recipients:
                 recipient_pending_leaves = []
-                recipient_company = models.EmploymentContract.objects.get(user=recipient).company
+                recipient_company = (
+                    models.EmploymentContract.objects.filter(
+                        Q(user=recipient)
+                        & Q(started_at__lte=date.today())
+                        & (Q(ended_at__gte=date.today()) | Q(ended_at__isnull=True))
+                    )
+                    .first()
+                    .company
+                )
                 for leave in pending_leaves:
-                    user_company = models.EmploymentContract.objects.get(user=leave.user).company
+                    user_company = (
+                        models.EmploymentContract.objects.filter(
+                            Q(user=leave.user)
+                            & Q(started_at__lte=date.today())
+                            & (Q(ended_at__gte=date.today()) | Q(ended_at__isnull=True))
+                        )
+                        .first()
+                        .company
+                    )
+
                     if user_company == recipient_company:
                         recipient_pending_leaves.append(leave)
                 if recipient.email:
-                    log.info('Sending reminder to %s' % recipient.email)
+                    log.info("Sending reminder to %s" % recipient.email)
 
                     send_mail(
                         recipient.email,
-                        _('Pending leave awaiting your approval'),
-                        'ninetofiver/emails/pending_leave_reminder.pug',
+                        _("Pending leave awaiting your approval"),
+                        "ninetofiver/emails/pending_leave_reminder.pug",
                         context={
-                            'user': recipient,
-                            'leaves': recipient_pending_leaves,
-                            'leave_ids': ','.join([str(x.id) for x in pending_leaves]),
-                            'leave_count': len(recipient_pending_leaves),
-                        }
+                            "user": recipient,
+                            "leaves": recipient_pending_leaves,
+                            "leave_ids": ",".join([str(x.id) for x in pending_leaves]),
+                            "leave_count": len(recipient_pending_leaves),
+                        },
                     )
