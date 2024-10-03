f928acdccab72cf20e836eb5e7f78f435ebe82b0
laurens <laurensoostrom@gmail.com>
2024-10-03 10:43:03 +0200

    Fix auth api issue

--- ninetofiver/authentication.py
+++ ninetofiver/authentication.py
@@ -10,29 +10,29 @@ class ApiKeyAuthentication(BaseTokenAuthentication):
 
     model = models.ApiKey
 
-    def authenticate(self, request):
-        """Authenticate the request."""
-        token = request.GET.get('api_key', None)
+    def authenticate(self, request): 
+        
+        # removed the following to prevent the use of query parameters for api_key
+        # token = request.GET.get('api_key', None) 
 
-        if not token:
-            auth = get_authorization_header(request).split()
+        """Authenticate the request.""" 
+        auth = get_authorization_header(request).split()
 
-            if auth and auth[0].lower() == self.keyword.lower().encode():
-                if len(auth) == 1:
-                    msg = _('Invalid token header. No credentials provided.')
-                    raise exceptions.AuthenticationFailed(msg)
-                elif len(auth) > 2:
-                    msg = _('Invalid token header. Token string should not contain spaces.')
-                    raise exceptions.AuthenticationFailed(msg)
+        if not auth or auth[0].lower() != self.keyword.lower().encode():
+            msg = _('Invalid token header. No credentials provided.')
+            raise exceptions.AuthenticationFailed(msg)
 
-                try:
-                    token = auth[1].decode()
-                except UnicodeError:
-                    msg = _('Invalid token header. Token string should not contain invalid characters.')
-                    raise exceptions.AuthenticationFailed(msg)
+        if len(auth) == 1:
+            msg = _('Invalid token header. No credentials provided.')
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = _('Invalid token header. Token string should not contain spaces.')
+            raise exceptions.AuthenticationFailed(msg)
 
-        if not token:
-            msg = _('Invalid token. No credentials provided.')
+        try:
+            token = auth[1].decode()
+        except UnicodeError:
+            msg = _('Invalid token header. Token string should not contain invalid characters.')
             raise exceptions.AuthenticationFailed(msg)
 
         res = self.authenticate_credentials(token)
